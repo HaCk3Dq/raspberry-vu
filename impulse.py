c5e991a6a79f135ea92ef85de2834f4b636f1cf8
HaCk3D <vladstudio@rambler.ru>
2016-06-05 14:53:27 +0300

    refactoring

--- impulse.py
+++ impulse.py
@@ -2,16 +2,28 @@
 
 import curses, time, impulse
 
+def init(window):
+  curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+  curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+  curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+  curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
+  curses.curs_set(0)
+  curses.use_default_colors()
+  window.nodelay(True)
+  window.clear()
+  window.refresh()
+
 def bar(window, x, y, height, value):
   stop = value * height
   for i in range(height, -1, -1):
     if i < stop:
       color = (i * 4 / height) + 1
       window.addstr(y + height - i, x, "    ", curses.color_pair(color) | curses.A_BOLD | curses.A_REVERSE)
-        
+    else:
+      window.addstr(y + height - i, x, "    ")
 
 def draw(window):
-  h,w = window.getmaxyx()
+  h, w = window.getmaxyx() # clear screen on resize
   audio_sample_array = impulse.getSnapshot(True)
   i = 0
   l = len(audio_sample_array) / 4
@@ -21,23 +33,12 @@ def draw(window):
     bar(window, x, 3, h - 6 , value)
     i += step
 
-
 def main(window):
-  curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
-  curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-  curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-  curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
-  curses.curs_set(0)
-
-  curses.use_default_colors()
-  window.nodelay(True)
-  window.clear()
-  window.refresh()
+  init(window)
   while window.getch() != 113 and window.getch() != 185:
     draw(window)
     window.refresh()
     time.sleep(0.05)
-    window.clear()
 
   window.clear()
   window.refresh()
