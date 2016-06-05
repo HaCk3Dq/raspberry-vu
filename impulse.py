4833c5ea6881ff872773559de31a734e9d5c4300
HaCk3D <vladstudio@rambler.ru>
2016-06-05 14:40:30 +0300

    proper resize redraw + indentation fix

--- impulse.py
+++ impulse.py
@@ -5,47 +5,44 @@ import time
 import impulse
 
 def bar(window, x, y, height, value):
-    stop = value * height
-    for i in range(height, -1, -1):
-        if i < stop:
-            color = (i * 4 / height) + 1
-            window.addstr(y + height - i, x, "    ", curses.color_pair(color) | curses.A_BOLD | curses.A_REVERSE)
-        else: window.addstr(y + height - i, x, "    ")
+  stop = value * height
+  for i in range(height, -1, -1):
+    if i < stop:
+      color = (i * 4 / height) + 1
+      window.addstr(y + height - i, x, "    ", curses.color_pair(color) | curses.A_BOLD | curses.A_REVERSE)
         
 
 def draw(window):
-    h,w = window.getmaxyx()
-    audio_sample_array = impulse.getSnapshot(True)
-    i = 0
-    l = len(audio_sample_array) / 4
-    sum = 0
-    step = l / ((w - 10) / 5)
-    for x in range(3, w - 5, 5):
-        value = audio_sample_array[i]
-        bar(window, x, 3, h - 6 , value)
-        sum += value
-        i += step
-    leds = 8 - min(8, int(sum * 2))
+  h,w = window.getmaxyx()
+  audio_sample_array = impulse.getSnapshot(True)
+  i = 0
+  l = len(audio_sample_array) / 4
+  step = l / ((w - 10) / 5)
+  for x in range(3, w - 5, 5):
+    value = audio_sample_array[i]
+    bar(window, x, 3, h - 6 , value)
+    i += step
 
 
 def main(window):
-    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.curs_set(0)
+  curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+  curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+  curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+  curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)
+  curses.curs_set(0)
 
-    curses.use_default_colors()
-    window.nodelay(True)
-    window.clear()
+  curses.use_default_colors()
+  window.nodelay(True)
+  window.clear()
+  window.refresh()
+  while 1:
+    draw(window)
     window.refresh()
-    while window.getch() < 0:
-        draw(window)
-        window.refresh()
-        time.sleep(0.05)
+    time.sleep(0.05)
+    window.clear()
 
-    window.clear()
-    window.refresh()
+  window.clear()
+  window.refresh()
 
 if __name__ == "__main__":
     curses.wrapper(main)
