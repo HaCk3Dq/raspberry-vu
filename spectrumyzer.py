9db3d5198deb89ae3671b2c6b0dfb6dfcfd48192
HaCk3D <vladstudio@rambler.ru>
2016-08-12 04:19:23 +0300

    parsing

--- spectrumyzer.py
+++ spectrumyzer.py
@@ -57,31 +57,37 @@ def render(window):
 # ===== Terminal Rendering =====
 
 def HexToRGB(value):
-  value = value.lstrip('#')
   lv = len(value)
-  return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+  byteValues = tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+  return (
+          round(byteValues[0]*(1/255.0),3),
+          round(byteValues[1]*(1/255.0),3),
+          round(byteValues[2]*(1/255.0),3),
+          )
 
-def parse(argList):
-  pass
+def parseConfig(argList):
+  if len(argList) != 10:
+    print "Error: invalid amount of arguments"
+    exit()
+  for e in ("-w", "-h", "-x", "-y", "-c"): argList.remove(e)
+  argList[4] = HexToRGB(argList[4])
+  print argList
+  window.set_size_request(600,400)
+  window.move(300,300)
 
 class Widget(Gtk.Window):
   def __init__(self):
     Gtk.Window.__init__(self, skip_pager_hint=True, skip_taskbar_hint=True)
     self.set_wmclass("sildesktopwidget","sildesktopwidget")
     self.set_type_hint(Gdk.WindowTypeHint.DESKTOP)
-    self.set_size_request(600,400)
     self.set_keep_below(True)
-
     screen = self.get_screen()
     rgba = screen.get_rgba_visual()
     self.set_visual(rgba)
     self.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(1,1,1,1))
-
     self.drawArea = Gtk.DrawingArea()
     self.drawArea.connect('draw', drawFreq)
     self.add(self.drawArea)
-
-    self.move(300,300)
     self.show_all()
 
 def drawFreq(widget, cr):
@@ -101,8 +107,9 @@ if __name__ == "__main__":
     winH = winW = int
     curses.wrapper(render)
   else:
-    parse(argList)
+    rgbaColor = [0, 0, 0]
     window = Widget()
+    parseConfig(argList)
     signal.signal(signal.SIGINT, signal.SIG_DFL) # make ^C work
     GLib.timeout_add(20, updateWindow, window)
     Gtk.main()
